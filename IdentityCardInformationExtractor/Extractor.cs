43eb52153edc51d900e9b0a5f1a0fb8cfa66185a
Erik Hudcovský <erik.hudcovsky@solarwinds.com>
2020-03-29 08:14:50 +0200

    príprava na vytvorenie nuget balíčka

---
+++ IdentityCardInformationExtractor/Extractor.cs
@@ -0,0 +1,62 @@
+﻿using IdentityCardInformationExtractor.Exceptions;
+using IdentityCardInformationExtractor.PapersOnProcess;
+using IdentityCardInformationExtractor.Interfaces;
+using System;
+
+namespace IdentityCardInformationExtractor
+{
+    public class Extractor
+    {
+        ///<sumary>
+        /// Process the given image with aditional information
+        ///</sumary>
+        ///<param name="cardType"> 
+        /// Card type (required) - (IC|P). Defines which type of card will be processed.
+        ///</param>
+        ///<param name="backSidePath">
+        /// Path to image with machine readable field (required)
+        ///</param>
+        ///<param name="format">
+        ///Format (optional, default is JSON) - (JSON|XML)
+        ///</param>
+        ///<param name="frontSidePath">
+        /// Path to other side of ID card (optional)
+        ///</param>
+        public static string process(string cardType, string backSidePath , string format = "JSON", string frontSidePath = null) 
+        {
+            try
+            {
+                switch (cardType)
+                {
+                    case "IC":
+                        var identificationCard = new IdentificationCardProcess(backSidePath,frontSidePath);
+                        return chooseOutput(format, identificationCard);
+
+                    case "P":
+                        var passport = new PassportProcess(backSidePath);
+                        return chooseOutput(format, passport);
+
+                    default:
+                        return "Card type was not recognized";
+                }
+            }
+            catch (PathToFileNotFoundException ex)
+            {
+                throw ex;
+            }
+        }
+
+        private static string chooseOutput(string format, ICardDataProcess cardDataProcess) 
+        {
+            switch (format)
+            {
+                case "JSON":
+                    return cardDataProcess.getIdentityCard().ToJson();
+                case "XML":
+                    return cardDataProcess.getIdentityCard().ToXml();
+                default:
+                    return cardDataProcess.getIdentityCard().ToJson();
+            }
+        }
+    }
+}
--- IdentityCardInformationExtractor/Program.cs
+++ IdentityCardInformationExtractor/Program.cs
@@ -1,100 +1,48 @@
 ﻿using System;
-using IdentityCardInformationExtractor.Exceptions;
-using IdentityCardInformationExtractor.Models;
-using IdentityCardInformationExtractor.PapersOnProcess;
-using IdentityCardInformationExtractor.Helpers;
-using IdentityCardInformationExtractor.Interfaces;
+using System.Collections.Generic;
+using System.Text;
 
 namespace IdentityCardInformationExtractor
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            //    "D:\\test\\HQ_TEST_OP.jpeg"
-
             if (args.Length < 2)
             {
-                Console.WriteLine("You have to pass atleast two parameters, check documentation for closer info");
+                Console.WriteLine("you have to pass atleast two parameters, check documentation for closer info");
                 Console.ReadLine();
             }
 
             if (args.Length == 2)
             {
-                var backSidePath = args[1];
-                var cardType = args[0];
-
-                process(cardType, backSidePath);
+                var backsidepath = args[1];
+                var cardtype = args[0];
+                Extractor.process(cardtype, backsidepath);
             }
 
             if (args.Length == 3)
             {
-                var backSidePath = args[1];
-                var cardType = args[0];
+                var backsidepath = args[1];
+                var cardtype = args[0];
                 var format = args[2];
 
-                process(cardType, backSidePath, format);
+                Extractor.process(cardtype, backsidepath, format);
             }
 
             if (args.Length == 4)
             {
-                var frontSidePath = args[3];
-                var backSidePath = args[1];
-                var cardType = args[0];
+                var frontsidepath = args[3];
+                var backsidepath = args[1];
+                var cardtype = args[0];
                 var format = args[2];
 
-                if (cardType == "P")
+                if (cardtype == "p")
                 {
-                    Console.WriteLine("The last parameter wasn't proceed because is not needed.");
+                    Console.WriteLine("the last parameter wasn't proceed because is not needed.");
                 }
 
-                process(cardType, backSidePath, format, frontSidePath);
-                
-            }
-        }
-
-        public static void process(string cardType, string backSidePath , string format = "JSON", string frontSidePath = null) 
-        {
-            try
-            {
-                switch (cardType)
-                {
-                    case "IC":
-                        var identificationCard = new IdentificationCardProcess(backSidePath,frontSidePath);
-                        IdentityCardHelper.Print(identificationCard.Text);
-                        Console.WriteLine(chooseOutput(format, identificationCard));
-                        break;
-
-                    case "P":
-
-                        var passport = new PassportProcess(backSidePath);
-                        IdentityCardHelper.Print(passport.Text);
-                        Console.WriteLine(chooseOutput(format, passport));
-                        break;
-
-                    default:
-                        Console.WriteLine("Card type was not recognized");
-                        break;
-                }
-            }
-            catch (PathToFileNotFoundException ex)
-            {
-                Console.WriteLine(ex);
-                Console.ReadKey();
-                throw;
-            }
-        }
-
-        public static string chooseOutput(string format, ICardDataProcess cardDataProcess) 
-        {
-            switch (format)
-            {
-                case "JSON":
-                    return cardDataProcess.getIdentityCard().ToJson();
-                case "XML":
-                    return cardDataProcess.getIdentityCard().ToXml();
-                default:
-                    return cardDataProcess.getIdentityCard().ToJson();
+                Extractor.process(cardtype, backsidepath, format, frontsidepath);
             }
         }
     }
